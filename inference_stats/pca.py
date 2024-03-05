8911a4648cfee93646b4e09a4cb07c7555070a1b
Sam Baumohl <28550422+sbaumohl@users.noreply.github.com>
2024-03-05 16:49:18 -0600

    pca via svd

--- inference_stats/pca.py
+++ inference_stats/pca.py
@@ -1,22 +1,21 @@
 from .base import PIPELINE
 from torch import Tensor
 from pandas import DataFrame, Series
-import einops
 import torch as t
+import einops
 
-# TODO replace with t.cov, depending on memory consumption
-def covar(x: t.Tensor) -> t.Tensor:
-    centered = (x - x.mean(dim=0))
-    return (centered.T @ centered) / (x.shape[0] - 1)
+def svd_pca(x: t.Tensor) -> t.Tensor:
+    x -= x.mean(0)
+    s = t.linalg.svdvals(x)
+    return t.square(s) / (x.size(0) - 1)
 
 @PIPELINE.register_filter()
 def class_covariance_eigvals(logits: Tensor, results: DataFrame) -> DataFrame:
     with t.no_grad():
-        
         for c in range(10):
-            concated_logits = logits[:, :, c].T
+            sliced_logits = logits[:, :, c].T
             
-            e_vals: t.Tensor = t.linalg.eigvalsh(covar(concated_logits))
+            e_vals: t.Tensor = svd_pca(sliced_logits)
             
             results[f'eig_vals_{c}'] = Series(e_vals.cpu().numpy())
     
@@ -35,9 +34,9 @@ def covariance_eigvals(logits: Tensor, results: DataFrame) -> DataFrame:
     """
     
     with t.no_grad():
-        concated_logits = einops.rearrange(logits, "i m l -> m (i l)")
+        model_stacked_logits = einops.rearrange(logits, "i m l -> m (i l)")
 
-        e_vals: t.Tensor = t.linalg.eigvalsh(covar(concated_logits))
+        e_vals: t.Tensor = svd_pca(model_stacked_logits)
 
         results['eig_vals'] = Series(e_vals.cpu().numpy())
     
