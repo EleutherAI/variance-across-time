0c62bcc53b8277e08ac8333970dd2f7a842bf2b1
Nora Belrose <belrose.nora@gmail.com>
2024-04-03 00:04:41 +0000

    Support cifar5m

--- train.py
+++ train.py
@@ -3,20 +3,25 @@ from contextlib import nullcontext
 from copy import deepcopy
 from functools import partial
 from pathlib import Path
+from typing import Callable, Generator
+import os
 import math
 import tempfile
 
+from PIL import Image
 from pytorch_lightning.loggers import WandbLogger
 from torch import nn, optim
 from torch.func import functional_call, stack_module_state
 from torch.optim.optimizer import Optimizer
-from torch.utils.data import random_split
+from torch.utils.data import Dataset, random_split
 from torchvision.datasets import CIFAR10
 from transformers import (
     ConvNextV2Config, ConvNextV2ForImageClassification,
     get_cosine_schedule_with_warmup,
 )
 
+import bitsandbytes as bnb
+import numpy as np
 import pytorch_lightning as pl
 import torch
 import torchvision.transforms as T
@@ -30,15 +35,13 @@ class Ensemble(pl.LightningModule):
     ):
         super().__init__()
         self.save_hyperparameters()
-
-        dtype = torch.float32
         self.models = nn.ModuleList()
 
         for offset in range(num_models):
             torch.cuda.manual_seed_all(seed + offset)
             torch.manual_seed(seed + offset)
 
-            self.models.append(self.build_net().to(dtype))
+            self.models.append(self.build_net())
 
     def build_net(self) -> nn.Module:
         cfg = ConvNextV2Config(
@@ -80,7 +83,7 @@ class Ensemble(pl.LightningModule):
             for name, param in model.named_parameters():
                 param.data = params[name][i]
 
-        opt = optim.AdamW(params.values(), lr=0.002, weight_decay=0.05)
+        opt = bnb.optim.Adam8bit(params.values(), lr=0.002, weight_decay=0.05)
         schedule = get_cosine_schedule_with_warmup(opt, 1_000, NUM_STEPS)
         return [opt], [{"scheduler": schedule, "interval": "step"}]
 
@@ -136,7 +139,7 @@ class Ensemble(pl.LightningModule):
             for i, model in enumerate(self.models):
                 # Weird thing we have to do to prevent PyTorch from saving the entire
                 # stack of models in the checkpoint instead of just the one we want
-                model = deepcopy(model)
+                model = deepcopy(model).bfloat16()
 
                 p = Path(f"{log_dir}/model_{i}/step={step}.pt")
                 p.parent.mkdir(parents=True, exist_ok=True)
@@ -154,13 +157,38 @@ class Ensemble(pl.LightningModule):
                     options=["-a"],
                 )
 
+
+class Cifar5m(Dataset):
+    def __init__(self, root: str, offset: int = 0, end: int = -1, transform: Callable = lambda x: x):
+        N = os.stat(f"{root}/X.bin").st_size // (32 * 32 * 3) 
+        num_images = max(end, N) - offset
+
+        self.transform = transform
+        self.X = np.memmap(
+            f"{root}/X.bin", dtype=np.uint8, mode='r', offset=offset * 32 * 32 * 3, shape=(num_images, 32, 32, 3)
+        )
+        self.Y = np.memmap(
+            f"{root}/Y.bin", dtype=np.uint64, mode='r', offset=offset, shape=(num_images,)
+        )
+
+    def __getitem__(self, idx) -> tuple[Image.Image, int]:
+        return self.transform(Image.fromarray(self.X[idx])), int(self.Y[idx])
+
+    def __len__(self):
+        return len(self.X)
+
+    def __iter__(self) -> Generator[tuple[Image.Image, int], None, None]:
+        yield from zip(map(self.transform, map(Image.fromarray, self.X)), map(int, self.Y))
+
+
 if __name__ == '__main__':
     # Use Tensor Cores even for float32
     torch.set_float32_matmul_precision("high")
 
     parser = ArgumentParser()
     parser.add_argument('name', type=str)
-    parser.add_argument('--num-models', type=int, default=32)
+    parser.add_argument('--cifar5m', action='store_true')
+    parser.add_argument('--num-models', type=int, default=64)
     parser.add_argument('--rsync-dest', type=str, default=None)
     parser.add_argument('--seed', type=int, default=0)
     args = parser.parse_args()
@@ -171,36 +199,45 @@ if __name__ == '__main__':
         T.RandAugment(),
         T.ToTensor(),
     ])
-    train = CIFAR10(
-        root='./cifar-train', train=True, download=True, transform=trf,
-    )
-    nontrain = CIFAR10(
-        root='./cifar-test', train=False, download=True, transform=T.ToTensor(),
-    )
+    if args.cifar5m:
+        train = Cifar5m('/weka/norabelrose/cifar-5m', transform=trf, end=5_992_688)
+        nontrain = Cifar5m('/weka/norabelrose/cifar-5m', offset=5_992_688, transform=T.ToTensor())
+    else:
+        train = CIFAR10(
+            root='/weka/norabelrose/variance-across-time/cifar-train', train=True, download=True, transform=trf,
+        )
+        nontrain = CIFAR10(
+            root='/weka/norabelrose/variance-across-time/cifar-test', train=False, download=True, transform=T.ToTensor(),
+        )
     # Use the fixed seed 0 to do the val-test split
+    # torch.set_default_dtype(torch.bfloat16)
     torch.manual_seed(0)
     val, test = random_split(nontrain, [0.1, 0.9])
 
     # Set the random seed used for initializing the models
-    torch.cuda.manual_seed_all(args.seed)
-    torch.manual_seed(args.seed)
+    seed = args.seed * args.num_models
+    torch.cuda.manual_seed_all(seed)
+    torch.manual_seed(seed)
 
-    print(f"\033[92m--- Using seed {args.seed} ---\033[0m")
+    print(f"\033[92m--- Using seed {seed} ---\033[0m")
 
     dm = pl.LightningDataModule.from_datasets(
         train, val, test, batch_size=64, num_workers=4,
     )
     ensemble = Ensemble(
-        args.seed, args.num_models, args.rsync_dest
+        seed, args.num_models, args.rsync_dest
     )
 
     trainer = pl.Trainer(
         logger=WandbLogger(
-            name=args.name, project="variance-across-time", entity="eleutherai"
+            name=args.name,
+            project="variance-across-time",
+            entity="eleutherai",
+            save_dir=f"warp_{args.seed}",
         ),
         max_steps=NUM_STEPS,
         # Mixed precision with (b)float16 activations
-        precision="16-mixed",
+        precision="bf16-mixed",
     )
     trainer.fit(ensemble, dm)
     trainer.test(ensemble, dm)
